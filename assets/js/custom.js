c77b3c474b6032a2b564b3261e1aae63f8f294e2
zhengzangw <zhengzangw@163.com>
2020-01-25 23:06:07 +0800

    refract some library

---
+++ assets/js/custom.js
@@ -0,0 +1,28 @@
+//katex auto loaded
+document.addEventListener("DOMContentLoaded", function () {
+    renderMathInElement(
+        document.body, {
+            delimiters: [{
+                    left: "$$",
+                    right: "$$",
+                    display: true
+                },
+                {
+                    left: "\\[",
+                    right: "\\]",
+                    display: true
+                },
+                {
+                    left: "$",
+                    right: "$",
+                    display: false
+                },
+                {
+                    left: "\\(",
+                    right: "\\)",
+                    display: false
+                }
+            ]
+        }
+    );
+});
---
+++ load.sh
@@ -0,0 +1,9 @@
+cp -r ./node_modules/bootswatch/dist/materia ./assets/sass/vendors-extensions/materia
+cp -r ./node_modules/bootstrap/scss ./assets/sass/vendors-extensions/bootstrap
+cp ./node_modules/animate.css/animate.css ./assets/sass/vendors-extensions/_animate.scss
+
+cp ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ./assets/js/bootstrap.min.js
+cp ./node_modules/jquery/dist/jquery.min.js ./assets/js/jquery.min.js
+cp ./node_modules/fuse.js/dist/fuse.js ./assets/js/fuse.js
+cp ./node_modules/katex/dist/katex.min.js ./assets/js/katex.min.js
+cp ./node_modules/katex/dist/contrib/auto-render.min.js ./assets/js/auto-render.min.js
